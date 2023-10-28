ef34276cf6e1812d137100ca6f360270feea853b
z3z1ma <butler.alex2010@gmail.com>
2023-10-28 15:39:49 -0700

    feat: more top level exports

--- src/cdf/__init__.py
+++ src/cdf/__init__.py
@@ -1,4 +1,6 @@
+import cdf.core.registry as registry
+from cdf.core.loader import DirectoryLoader
 from cdf.core.source import resource as cdf_resource
 from cdf.core.source import source as cdf_source
 
-__all__ = ["cdf_source", "cdf_resource"]
+__all__ = ["cdf_source", "cdf_resource", "registry", "DirectoryLoader"]
