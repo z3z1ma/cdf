8a603b8246d8c76fac88dcd42bab4ab946ac165d
z3z1ma <butler.alex2010@gmail.com>
2023-10-30 12:36:48 -0700

    chore: add docstrings to loader module

--- src/cdf/core/loader.py
+++ src/cdf/core/loader.py
@@ -19,7 +19,18 @@ from cdf.core.utils import _augmented_path
 
 
 def get_directory_modules(base_directory: Path | str) -> t.Iterable[ct.Loadable]:
-    """Load all modules in the sources directory."""
+    """Load all modules in the sources directory.
+
+    Args:
+        base_directory (Path | str): The base directory to load modules from.
+
+    Raises:
+        SourceDirectoryNotFoundError: If the base directory does not exist.
+        SourceDirectoryNotFoundError: If the base directory is not a directory.
+
+    Returns:
+        t.Iterable[ct.Loadable]: An iterable of modules.
+    """
     if isinstance(base_directory, str):
         base_directory = Path(base_directory)
     if not base_directory.exists():
@@ -35,6 +46,14 @@ def get_directory_modules(base_directory: Path | str) -> t.Iterable[ct.Loadable]
 
 
 def _load_module_from_path(path: Path) -> ModuleType:
+    """Load a module from a path.
+
+    Args:
+        path (Path): The path to the module.
+
+    Returns:
+        ModuleType: The loaded module.
+    """
     spec = importlib.util.spec_from_file_location(path.stem, path)
     assert spec and spec.loader, f"Failed to create spec for {path}"
     mod = importlib.util.module_from_spec(spec)
@@ -43,10 +62,29 @@ def _load_module_from_path(path: Path) -> ModuleType:
 
 
 def _load_module_from_name(name: str) -> ModuleType:
+    """Load a module from a name.
+
+    Args:
+        name (str): The name of the module as an import path.
+
+    Returns:
+        ModuleType: The loaded module.
+    """
     return importlib.import_module(name)
 
 
 def load_module(mod: ct.Loadable) -> ModuleType:
+    """Load a module.
+
+    Args:
+        mod (ct.Loadable): The module to load.
+
+    Raises:
+        TypeError: If the module is not a valid type.
+
+    Returns:
+        ModuleType: The loaded module.
+    """
     if isinstance(mod, ModuleType):
         result = ct.Result(mod)
     elif isinstance(mod, str):
@@ -71,6 +109,26 @@ def load_sources(
     lazy_sources: bool = True,
     clear_linecache: bool = True,
 ) -> None:
+    """Load all sources from the sources directory.
+
+    Args:
+        get_modules_fn (t.Callable[[], t.Iterable[ct.Loadable]], optional): A function
+            that returns an iterable of modules. Defaults to partial(get_directory_modules, Path("./sources")).
+        load_module_fn (t.Callable[[ct.Loadable], ModuleType], optional): A function that
+            loads a module. Defaults to load_module.
+        cache (ct.SourceSpec | None, optional): A dict to cache sources in. Defaults to None. This is
+            mutable, so it is modified in-place.
+        lazy_sources (bool, optional): Whether to lazy load sources. Defaults to True.
+        clear_linecache (bool, optional): Whether to clear the linecache. Defaults to True.
+
+    Raises:
+        SourceDirectoryNotFoundError: If the base directory does not exist.
+        SourceDirectoryNotFoundError: If the base directory is not a directory.
+        SourceDirectoryEmpty: If the base directory contains no sources.
+
+    Returns:
+        None: Nothing.
+    """
     cache = cache if cache is not None else {}
     if clear_linecache:
         linecache.clearcache()
