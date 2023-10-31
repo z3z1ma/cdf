a3aca99ebcdbc35e3c01eb84c27ca30ab15cab80
z3z1ma <butler.alex2010@gmail.com>
2023-10-30 20:17:03 -0700

    feat: add some util tests

---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+import sys
+
+from cdf.core.utils import _augmented_path, do
+
+
+def test_append_syspath(mocker):
+    """Test that _augmented_path appends to sys.path."""
+    mocker.patch.object(sys, "path", new=[])
+
+    # Test case 1: Append a path to sys.path
+    with _augmented_path("./tests/fixtures"):
+        assert "./tests/fixtures" in sys.path
+    # Test case 2: Append multiple paths to sys.path
+    with _augmented_path("./tests/fixtures", "./tests/fixtures/empty"):
+        assert "./tests/fixtures" in sys.path
+        assert "./tests/fixtures/empty" in sys.path
+    # Test case 3: Append a path to sys.path, ensure it is removed
+    with _augmented_path("./tests/fixtures"):
+        pass
+    assert "./tests/fixtures" not in sys.path
+
+
+def test_do():
+    assert do(lambda x: x + 1, [1, 2, 3]) == [2, 3, 4]
