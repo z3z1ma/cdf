a83c9fb971ce40edd9d6d04bf794cdcbb27c600f
z3z1ma <butler.alex2010@gmail.com>
2023-10-30 20:21:43 -0700

    feat: add basic cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,19 @@
+from typer.testing import CliRunner
+
+from cdf.cli import app
+
+runner = CliRunner()
+
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_index():
+    result = runner.invoke(app, ["-p", "./tests/fixtures/sources", "index"])
+    assert result.exit_code == 0
+    assert "source1" in result.stdout
+
+    result = runner.invoke(app, ["-p", "./tests/fixtures/empty", "index"])
+    assert result.exit_code == 1
