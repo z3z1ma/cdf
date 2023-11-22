bc78309fb3762239e614779e45928b8b379f4f37
z3z1ma <butler.alex2010@gmail.com>
2023-11-22 14:44:11 -0700

    chore: add generic typing for publisher

--- src/cdf/core/publisher.py
+++ src/cdf/core/publisher.py
@@ -1,10 +1,15 @@
 import typing as t
 from dataclasses import dataclass
 
+from dlt.common.configuration import with_config
+from dlt.common.configuration.specs.base_configuration import BaseConfiguration
+
+T = t.TypeVar("T", bound=BaseConfiguration)
+
 
 @dataclass
-class CDFPublisherWrapper:
-    runner: t.Callable[..., None]
+class CDFPublisherWrapper(t.Generic[T]):
+    runner: t.Callable[[T], None]
     from_model: str
     mapping: t.Dict[str, str]
     version: int = 1
@@ -13,3 +18,14 @@ class CDFPublisherWrapper:
     tags: t.Sequence[str] = ()
     cron: str | None = None
     enabled = True
+    config: t.Type[T] = t.Any
+
+    def __post_init__(self) -> None:
+        runner = self.runner
+
+        @with_config(spec=self.config, sections=("publishers", runner.__name__))
+        def _runner(config: T) -> None:
+            runner(config)
+
+        _runner.__wrapped__ = runner
+        self.runner = _runner
