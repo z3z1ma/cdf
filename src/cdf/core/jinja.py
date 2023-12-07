3eeee0256b285ccc6beb56d96b4a61ee88e23a9d
z3z1ma <butler.alex2010@gmail.com>
2023-12-07 00:04:06 -0700

    feat: add serializers to config renderer

--- src/cdf/core/jinja.py
+++ src/cdf/core/jinja.py
@@ -1,9 +1,15 @@
 import base64
+import io
+import json
 import os
 from datetime import datetime, timedelta
 
 import jinja2
 import jinja2.ext
+import ruamel.yaml
+import tomlkit
+
+YAML = ruamel.yaml.YAML(typ="safe")
 
 
 class ShortEnvExtension(jinja2.ext.Extension):
@@ -23,6 +29,18 @@ ENVIRONMENT = jinja2.Environment(
     extensions=["jinja2.ext.do", "jinja2.ext.loopcontrols", ShortEnvExtension],
 )
 
+
+def _to_yaml(obj):
+    buf = io.StringIO()
+    YAML.dump(obj, buf)
+    return buf.getvalue()
+
+
+def _from_yaml(s):
+    buf = io.StringIO(s)
+    return YAML.load(buf)
+
+
 JINJA_METHODS = {
     "env_var": lambda key, default=None: os.getenv(key, default),
     "today": lambda: datetime.now().strftime("%Y-%m-%d"),
@@ -34,8 +52,14 @@ JINJA_METHODS = {
     "days_later": lambda n: (datetime.now() + timedelta(days=n)).strftime("%Y-%m-%d"),
     "weeks_ago": lambda n: (datetime.now() - timedelta(weeks=n)).strftime("%Y-%m-%d"),
     "weeks_later": lambda n: (datetime.now() + timedelta(weeks=n)).strftime("%Y-%m-%d"),
-    "base64": lambda s: base64.b64encode(s.encode("utf-8")).decode("utf-8"),
-    "base64_decode": lambda s: base64.b64decode(s.encode("utf-8")).decode("utf-8"),
+    "b64encode": lambda s: base64.b64encode(s.encode("utf-8")).decode("utf-8"),
+    "b64decode": lambda s: base64.b64decode(s.encode("utf-8")).decode("utf-8"),
+    "to_json": lambda s: json.dumps(s),
+    "from_json": lambda s: json.loads(s),
+    "to_yaml": _to_yaml,
+    "from_yaml": _from_yaml,
+    "to_toml": lambda s: tomlkit.dumps(s),
+    "from_toml": lambda s: tomlkit.loads(s),
 }
 """
 Methods available to the config rendering context.
