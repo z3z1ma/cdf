3e78f2f96c710f3efd11c5cf578c9c8df48cf915
z3z1ma <butler.alex2010@gmail.com>
2023-12-11 00:10:25 -0700

    feat: more config rendering conveniences

--- src/cdf/core/jinja.py
+++ src/cdf/core/jinja.py
@@ -1,7 +1,9 @@
+import ast
 import base64
 import io
 import json
 import os
+import typing as t
 from datetime import datetime, timedelta
 
 import jinja2
@@ -11,23 +13,29 @@ import tomlkit
 
 YAML = ruamel.yaml.YAML(typ="safe")
 
+T = t.TypeVar("T", dict, list, str)
 
-def _to_yaml(obj: dict) -> str:
+
+def _to_yaml(obj: T) -> str:
+    """Dumps a dictionary to a YAML string."""
     buf = io.StringIO()
     YAML.dump(obj, buf)
     return buf.getvalue()
 
 
-def _from_yaml(s: str) -> dict:
+def _from_yaml(s: str) -> T:
+    """Loads a YAML string into a dictionary."""
     buf = io.StringIO(s)
     return YAML.load(buf)
 
 
 def _escape_linefeeds(s: str) -> str:
+    """Escapes linefeeds in a string."""
     return s.replace("\n", "\\n")
 
 
-def _recursive_escape_linefeeds(obj: dict | list) -> dict | list:
+def _recursive_escape_linefeeds(obj: T) -> T:
+    """Recursively escape linefeeds in a dictionary or string."""
     if isinstance(obj, dict):
         return {k: _recursive_escape_linefeeds(v) for k, v in obj.items()}
     elif isinstance(obj, list):
@@ -35,6 +43,18 @@ def _recursive_escape_linefeeds(obj: dict | list) -> dict | list:
     elif isinstance(obj, str):
         return _escape_linefeeds(obj)
     return obj
+
+
+def _cat(path: str) -> str:
+    """Reads a file into a string."""
+
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def _head(input: str, n: int = 10) -> str:
+    """Returns the first n lines of a string."""
+    return "\n".join(input.split("\n")[:n])
 
 
 class CDFExtension(jinja2.ext.Extension):
@@ -47,14 +67,24 @@ class CDFExtension(jinja2.ext.Extension):
         environment.filters["totoml"] = lambda s: tomlkit.dumps(s)
         environment.filters["fromtoml"] = lambda s: tomlkit.loads(s)
         environment.filters["env"] = lambda s, default=None: os.getenv(s, default)
+        environment.filters["b64encode"] = lambda s: base64.b64encode(
+            s.encode("utf-8")
+        ).decode("utf-8")
+        environment.filters["b64decode"] = lambda s: base64.b64decode(
+            s.encode("utf-8")
+        ).decode("utf-8")
+        environment.filters["head"] = _head
+        environment.filters["eval"] = lambda s: ast.literal_eval(s)
 
 
-def _finalize(s: str) -> str:
-    if isinstance(s, (dict, list)):
-        return _to_toml(_recursive_escape_linefeeds(s))
-    elif isinstance(s, str):
-        return _escape_linefeeds(s)
-    return s
+def _finalize(s: T) -> str:
+    """Converts native types to TOML, escaping linefeeds."""
+    v = _recursive_escape_linefeeds(s)
+    if isinstance(v, dict):
+        return tomlkit.dumps(v)
+    if isinstance(v, list):
+        return tomlkit.dumps({"_": v})[4:-1]
+    return v
 
 
 ENVIRONMENT = jinja2.Environment(
@@ -75,8 +105,7 @@ JINJA_CONTEXT = {
     "days_later": lambda n: (datetime.now() + timedelta(days=n)).strftime("%Y-%m-%d"),
     "weeks_ago": lambda n: (datetime.now() - timedelta(weeks=n)).strftime("%Y-%m-%d"),
     "weeks_later": lambda n: (datetime.now() + timedelta(weeks=n)).strftime("%Y-%m-%d"),
-    "b64encode": lambda s: base64.b64encode(s.encode("utf-8")).decode("utf-8"),
-    "b64decode": lambda s: base64.b64decode(s.encode("utf-8")).decode("utf-8"),
+    "cat": _cat,
     "workspace": None,
     "root": None,
 }
