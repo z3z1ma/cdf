90ed34b8d784df8846379b87e12b47b51a275336
z3z1ma <butler.alex2010@gmail.com>
2024-03-28 20:57:39 -0500

    feat: add path arg and dirfs wrapper for namespacing fs provider

--- src/cdf/core/filesystem.py
+++ src/cdf/core/filesystem.py
@@ -3,21 +3,28 @@
 import typing as t
 
 import fsspec
+import fsspec.implementations.dirfs as dirfs
 from dlt.common.configuration import with_config
 
 
 @with_config(sections=("filesystem",))
 def load_filesystem_provider(
-    provider: t.Optional[str] = None, options: t.Optional[t.Dict[str, t.Any]] = None
+    provider: t.Optional[str] = None,
+    path: t.Optional[str] = None,
+    options: t.Optional[t.Dict[str, t.Any]] = None,
 ) -> fsspec.AbstractFileSystem:
     """Load a filesystem from a provider and kwargs.
 
     Args:
         provider: The filesystem provider.
-        kwargs: The filesystem provider kwargs.
+        options: The filesystem provider kwargs.
 
     Returns:
         The filesystem.
     """
-    options = options or {}
-    return fsspec.filesystem(provider, **options)
+    return dirfs.DirFileSystem(
+        path,
+        target_protocol=provider or "file",
+        target_options=options or {},
+        auto_mkdir=True,
+    )
