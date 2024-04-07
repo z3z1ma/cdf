559f158012c874a52122c01a7e299a61ac90faf0
z3z1ma <butler.alex2010@gmail.com>
2024-04-07 13:01:50 -0700

    chore: add cli sanity test

---
+++ tests/test_cli.py
@@ -0,0 +1,12 @@
+from typer.testing import CliRunner
+
+from cdf.cli import app
+
+runner = CliRunner()
+
+
+def test_index():
+    result = runner.invoke(app, ["-p", "examples/sandbox", "alex", "index"])
+    assert result.exit_code == 0
+    assert "Pipelines" in result.stdout
+    assert "Sinks" in result.stdout
