1376ebb103b2b7e9372ae6ca66532a15d42618fe
z3z1ma <butler.alex2010@gmail.com>
2024-04-07 11:36:28 -0700

    chore: cleanup tests since interface changes

--- tests/core/test_configuration.py
+++ tests/core/test_configuration.py
@@ -17,20 +17,4 @@ def test_load_config():
     project = project.unwrap()
 
     # Project config can be indexed directly, this gets the project name
-    assert project["name"] == "cdf-test"
-
-    # Namespaced access by indexing with a tuple (workspace, key)
-    assert project[("workspace1", "name")] == "workspace1"
-
-    # Namespaced access falls back to the project settings
-    # in this way, workspace settings can override project settings if needed
-    # This is the expected access pattern
-    assert project[("workspace1", "filesystem.provider")] == "local"
-
-    # Access by attribute works too, but does not support the
-    # behavior of falling back to the project settings since we are directly accessing
-    # the workspace settings
-    assert (
-        project[("workspace1", "pipelines.us_cities")]
-        == project.workspace1.pipelines.us_cities
-    )
+    assert project["project"]["name"] == "cdf-test"
